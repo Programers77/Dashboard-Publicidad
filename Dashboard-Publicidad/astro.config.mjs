1c4648373562837496abf1dc75cdfce8629f9284
Gilberto <gilbertojaviermb1994@gmail.com>
2025-05-07 09:07:52 -0400

    Ultimos cambios

--- Dashboard-Publicidad/astro.config.mjs
+++ Dashboard-Publicidad/astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      host: '0.0.0.0', // Permite conexiones desde cualquier IP en la red local
+      port: 4321, // Puerto por defecto de Astro (puedes cambiarlo)
+      strictPort: true, // Evita que Vite cambie el puerto si está ocupado
+    },
+  },
+});
